afa0ea36e0668d67ada482c8b175ca71a2817e42
Harsh Kumar singh <115934283+harsh-kumar-singh-code@users.noreply.github.com>
2025-04-11 12:08:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+const links = navLinks.querySelectorAll('a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
+
